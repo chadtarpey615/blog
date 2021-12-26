ac886b0787e6d17cfcc1b0b581bf7f953f20fb9e
Chad Tarpey <chadtarpey@gmail.com>
2021-12-26 17:49:59 -0600

    created bootstrap navbar for friends app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  
-  resources :articles do
-    resources :comments
-  end
+  root  "home#index"
+  get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
